0a83770a4310e947a94e83a41d8e31429200af55
georgiesamaha <73086054+georgiesamaha@users.noreply.github.com>
2020-11-26 11:15:46 +1100

    Create intermarkerdistance.sh

---
+++ intermarkerdistance.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+
+# calculates intermarker distance and maximum distance for a list of snps.
+# input is bim format, for vcf change $4 to $2 in awk command 
+# where C= chr/contig, P=previous marker position, M= max distance, T= total length, N= number variants, L=current distance
+# run as intermarkerdistance.sh <filename>
+
+function input(){
+for chr in {1..18};
+	do
+	
+	awk -v chr=$chr '$1==chr {print}' $@ | awk 'BEGIN{C="";P=-1;M=0;T=0.0;N=0;} /^#/{next} {if(C==$1) {L=int($4)-P;T+=L;N++;M=(M>L?M:L);}C=$1;P=int($4);}END{if(N>0) printf(C" avg=%f max=%d\n",T/N,M);}'
+
+done;
+}
+
+input $@
