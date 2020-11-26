f4aa8de8911cd69469a6dfa47b14514a0eb11b01
georgiesamaha <73086054+georgiesamaha@users.noreply.github.com>
2020-11-26 11:35:30 +1100

    Update intermarkerdistance.sh

--- intermarkerdistance.sh
+++ intermarkerdistance.sh
@@ -4,13 +4,13 @@
 # input is bim format, for vcf change $4 to $2 in awk  
 # where C= chr/contig, P=previous marker position, M= max distance, T= total length, N= number variants, L=current distance
 # run as intermarkerdistance.sh <filename>
-# output as chr/avg_intermarker_dist/maximum_dist per chromosome
+# output as chr/marker_count/avg_intermarker_dist/maximum_dist per chromosome
 
 function input(){
 for chr in {1..18};
 	do
 	
-	awk -v chr=$chr '$1==chr {print}' $@ | awk 'BEGIN{C="";P=-1;M=0;T=0.0;N=0;} /^#/{next} {if(C==$1) {L=int($4)-P;T+=L;N++;M=(M>L?M:L);}C=$1;P=int($4);}END{if(N>0) printf(C" avg=%f max=%d\n",T/N,M);}'
+	awk -v chr=$chr '$1==chr {print}' $@ | awk 'BEGIN{C="";P=-1;M=0;T=0.0;N=0;} /^#/{next} {if(C==$1) {L=int($4)-P;T+=L;N++;M=(M>L?M:L);}C=$1;P=int($4);}END{if(N>0) printf(C" count=%d avg=%f max=%d\n",N,T/N,M);}'
 
 done;
 }
