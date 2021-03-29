51013e11a10e7e4232dc3a67386253b6c6892d35
georgiesamaha <73086054+georgiesamaha@users.noreply.github.com>
2021-03-29 16:14:02 +1100

    Update bam_depth.sh

--- bam_depth.sh
+++ bam_depth.sh
@@ -1,5 +1,7 @@
 #!/bin/bash 
 
 # Calculates average read depth of a bam using samtools and prints to screen
+# 
+# 
 
 samtools depth -a final.bam  |  awk '{sum+=$3; sumsq+=$3*$3} END { print "Average = ",sum/NR; print "Stdev = ",sqrt(sumsq/NR - (sum/NR)**2)}' 
